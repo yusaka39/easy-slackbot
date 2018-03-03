78803d6e8610084971fba5d2d8b1772389cd7778
Yuta Sakata <yusaka39@gmail.com>
2018-03-03 19:28:18 +0900

    Add tests for extensions

--- src/test/kotlin/io/github/yusaka39/easySlackbot/lib/ExtensionsTest.kt
+++ src/test/kotlin/io/github/yusaka39/easySlackbot/lib/ExtensionsTest.kt
@@ -1,10 +1,25 @@
 package io.github.yusaka39.easySlackbot.lib
 
+import io.github.yusaka39.easySlackbot.slack.Attachment
+import io.github.yusaka39.easySlackbot.slack.Channel
+import io.github.yusaka39.easySlackbot.slack.Message
+import io.github.yusaka39.easySlackbot.slack.User
 import org.junit.Test
+import org.riversun.slacklet.SlackletRequest
+import org.riversun.xternal.simpleslackapi.SlackAttachment
+import org.riversun.xternal.simpleslackapi.SlackBot
+import org.riversun.xternal.simpleslackapi.SlackChannel
+import org.riversun.xternal.simpleslackapi.SlackFile
+import org.riversun.xternal.simpleslackapi.SlackPersona
+import org.riversun.xternal.simpleslackapi.SlackUser
+import org.riversun.xternal.simpleslackapi.events.SlackEventType
+import org.riversun.xternal.simpleslackapi.events.SlackMessagePosted
+import java.util.ArrayList
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 import kotlin.reflect.full.starProjectedType
 import kotlin.reflect.full.withNullability
+import kotlin.reflect.jvm.isAccessible
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertFalse
@@ -161,6 +176,230 @@ class ExtensionsTest {
         @AnnotationForTesting2
         class Bar
         assertFalse { Bar::class.isAnnotatedWith<AnnotationForTesting2>() }
+    }
+
+    private class TestChannel : SlackChannel {
+        override fun getPurpose(): String {
+            TODO("not implemented")
+        }
+
+        override fun isArchived(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun getName(): String = "general"
+
+        override fun getId(): String = "Cgeneral"
+
+        override fun getType(): SlackChannel.SlackChannelType {
+            TODO("not implemented")
+        }
+
+        override fun isDirect(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun getMembers(): MutableCollection<SlackUser> {
+            TODO("not implemented")
+        }
+
+        override fun isMember(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun getTopic(): String {
+            TODO("not implemented")
+        }
+    }
+
+    @Test
+    fun toChannelConvertSlackChannelToChannel() {
+        val chan = TestChannel()
+        assertEquals(Channel("Cgeneral", "general"), chan.toChannel())
+    }
+
+    private class TestUser : SlackUser {
+        override fun getUserSkype(): String {
+            TODO("not implemented")
+        }
+
+        override fun isOwner(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun isDeleted(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun isBot(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun getTimeZone(): String {
+            TODO("not implemented")
+        }
+
+        override fun getUserName(): String = "awesomekotlin"
+
+        override fun getId(): String = "Uabcdefg"
+
+        override fun getTimeZoneLabel(): String {
+            TODO("not implemented")
+        }
+
+        override fun getUserTitle(): String {
+            TODO("not implemented")
+        }
+
+        override fun isUltraRestricted(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun getUserMail(): String {
+            TODO("not implemented")
+        }
+
+        override fun getUserPhone(): String {
+            TODO("not implemented")
+        }
+
+        override fun getTimeZoneOffset(): Int {
+            TODO("not implemented")
+        }
+
+        override fun isPrimaryOwner(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun isRestricted(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun getPresence(): SlackPersona.SlackPresence {
+            TODO("not implemented")
+        }
+
+        override fun isAdmin(): Boolean {
+            TODO("not implemented")
+        }
+
+        override fun getRealName(): String = "John Doe"
+
+    }
+
+    @Test
+    fun toUserConvertSlackUserToUser() {
+        val user = TestUser()
+        assertEquals(User("Uabcdefg", "awesomekotlin", "John Doe"), user.toUser())
+    }
+
+    @Test
+    fun toSlackAttachmentConvertAttachmentToSlackAttachment() {
+        val slackAttachment = Attachment(
+            "fallback",
+            "color",
+            "authorName",
+            "authorLink",
+            "authorIcon",
+            "title",
+            "titleLink",
+            "text",
+            "preText",
+            "imageUrl",
+            "thumbnailUrl",
+            "footer",
+            "footerIcon",
+            listOf(
+                Attachment.Field("title", "value", true)
+            ),
+            listOf(
+                Attachment.Action("type", "text", "url")
+            )
+        ).toSlackAttachment()
+
+        assertEquals("fallback", slackAttachment.fallback)
+        assertEquals("color", slackAttachment.color)
+        assertEquals("authorName", slackAttachment.authorName)
+        assertEquals("authorLink", slackAttachment.authorLink)
+        assertEquals("authorIcon", slackAttachment.authorIcon)
+        assertEquals("title", slackAttachment.title)
+        assertEquals("titleLink", slackAttachment.titleLink)
+        assertEquals("text", slackAttachment.text)
+        assertEquals("preText", slackAttachment.pretext)
+        assertEquals("imageUrl", slackAttachment.imageUrl)
+        assertEquals("thumbnailUrl", slackAttachment.thumbUrl)
+        assertEquals("footer", slackAttachment.footer)
+        assertEquals("footerIcon", slackAttachment.footerIcon)
+
+        assertEquals(1, slackAttachment.fields.size)
+        slackAttachment.fields[0].let {
+            assertEquals("title", it.title)
+            assertEquals("value", it.value)
+            assertEquals(true, it.isShort)
+        }
+
+        assertEquals(1, slackAttachment.actions.size)
+        slackAttachment.actions[0].let {
+            assertEquals("type", it.type)
+            assertEquals("text", it.text)
+            assertEquals("url", it.value)
+        }
+    }
+
+    @Test
+    fun getMessageReturnsCorrectMessage() {
+        val postedMessage = object : SlackMessagePosted {
+            override fun getTimeStamp(): String = "12345678.123456"
+
+            override fun getSender(): SlackUser = TestUser()
+
+            override fun getMessageContent(): String = "kotlin is awesome."
+
+            override fun getChannel(): SlackChannel = TestChannel()
+
+            override fun getMessageSubType(): SlackMessagePosted.MessageSubType {
+                TODO("not implemented")
+            }
+
+            override fun getJsonSource(): String {
+                TODO("not implemented")
+            }
+
+            override fun getSlackFile(): SlackFile {
+                TODO("not implemented")
+            }
+
+            override fun getReactions(): MutableMap<String, Int> {
+                TODO("not implemented")
+            }
+
+            override fun getBot(): SlackBot {
+                TODO("not implemented")
+            }
+
+            override fun getEventType(): SlackEventType {
+                TODO("not implemented")
+            }
+
+            override fun getTimestamp(): String = this.timeStamp
+
+            override fun getTotalCountOfReactions(): Int {
+                TODO("not implemented")
+            }
+
+            override fun getAttachments(): ArrayList<SlackAttachment> {
+                TODO("not implemented")
+            }
+
+        }
+        val request = SlackletRequest::class.constructors.first().let {
+            it.isAccessible = true
+            it.call(null, postedMessage, null)
+        }
+        assertEquals(
+            Message(TestUser().toUser(), "kotlin is awesome.", TestChannel().toChannel(), "12345678.123456"),
+            request.getMessage()
+        )
     }
 }
 
