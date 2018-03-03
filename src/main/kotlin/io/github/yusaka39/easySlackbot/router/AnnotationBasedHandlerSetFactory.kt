94e6b87374d1de50a841baa2ae746d88f63eddce
Yuta Sakata <yusaka39@gmail.com>
2018-03-03 14:56:07 +0900

    Improve reflective operation

--- src/main/kotlin/io/github/yusaka39/easySlackbot/router/AnnotationBasedHandlerSetFactory.kt
+++ src/main/kotlin/io/github/yusaka39/easySlackbot/router/AnnotationBasedHandlerSetFactory.kt
@@ -9,12 +9,18 @@ class AnnotationBasedHandlerSetFactory(private val packageName: String) : Handle
     @Suppress("unchecked_cast")
     override fun create(): Set<Handler> =
             ClassPath.from(ClassLoader.getSystemClassLoader()).getTopLevelClassesRecursive(this.packageName).flatMap {
-                val kClass = it.load().kotlin
-                kClass.members.flatMap inner@ { kCallable ->
-                    val annotation = kCallable.findAnnotation<HandlerFunction>() ?: return@inner emptyList<Handler>()
-                    annotation.type.map {
-                        Handler(kClass, kCallable, annotation.regex.toRegex(annotation.regexOption.toSet()), it)
+                try {
+                    val kClass = it.load().kotlin
+                    kClass.members.flatMap inner@ { kCallable ->
+                        val annotation = kCallable.findAnnotation<HandlerFunction>()
+                                ?: return@inner emptyList<Handler>()
+                        annotation.type.map {
+                            Handler(kClass, kCallable, annotation.regex.toRegex(annotation.regexOption.toSet()), it)
+                        }
                     }
+                } catch (e: UnsupportedOperationException) {
+                    // TODO: Show warning
+                    emptyList<Handler>()
                 }
             }.toSet()
 }
