7d1e357a7ebdf4b8b5d2238f50919e95cbc923e3
Yuta Sakata <yusaka39@gmail.com>
2018-04-11 16:07:57 +0900

    Add FutureAction

--- src/main/kotlin/io/github/yusaka39/easySlackbot/router/actions/Action.kt
+++ src/main/kotlin/io/github/yusaka39/easySlackbot/router/actions/Action.kt
@@ -3,6 +3,12 @@ package io.github.yusaka39.easySlackbot.router.actions
 import io.github.yusaka39.easySlackbot.slack.Slack
 
 interface Action {
+    companion object {
+        fun create(procedure: (Slack) -> Unit) = object : Action {
+            override fun run(slack: Slack) = procedure(slack)
+        }
+    }
+
     fun run(slack: Slack)
     infix fun compose(doAfter: Action): Action = object : Action {
         override fun run(slack: Slack) {
